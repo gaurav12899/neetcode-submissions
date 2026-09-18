6a85cefd84250aad2d5e5914757bbcd60a4a06d5
Gaurav Jain <44408208+gaurav12899@users.noreply.github.com>
2026-09-18 22:19:13 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.kt
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        if(root==null || root == p || root == q)return root
+        val left = lowestCommonAncestor(root.left,p,q)
+        val right = lowestCommonAncestor(root.right,p,q)
+        if(left!=null && right!=null)return root
+        return left?:right
+    }
+}
